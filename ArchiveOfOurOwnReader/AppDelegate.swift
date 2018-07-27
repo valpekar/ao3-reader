5f5d3956aaf52c6c2654894359d9014c7eeb1bf7
Valeriya Pekar <no email>
2018-07-27 18:46:06 +0300

    small fix;

--- ArchiveOfOurOwnReader/AppDelegate.swift
+++ ArchiveOfOurOwnReader/AppDelegate.swift
@@ -308,8 +308,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        self.saveContext()
+      //  self.saveContext()
     }
+    
+    
     
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
